40644408070bc615d34f1b292ba91852e1874178
GG <weebcyberpunk@gmail.com>
2022-03-05 21:37:00 -0300

    Added todo.h

--- main.c
+++ main.c
@@ -13,9 +13,10 @@ int main(int argv, char *argc[]) {
 	fp = fopen(*home);
 
 	// eval args
-	if ((argc[1] == "--list") || (argc[1] == "-l")) list(fp);
-	else if ((argc[1] == "--create") || (argc[1] == "-c")) create(argc[2], fp);
-	else if ((argc[1] == "--delete") || (argc[1] == "-d")) delete(argc[2], fp);
+	if ((argc[1] == "--list") || (argc[1] == "-l")) list_tasks(fp);
+	else if ((argc[1] == "--create") || (argc[1] == "-c")) create_task(argc[2], fp);
+	else if ((argc[1] == "--delete") || (argc[1] == "-d")) delete_task(argc[2], fp);
+	else if (argc[1] == "--help") todo_help();
 	else list();
 	
 	return(0);
---
+++ todo.h
@@ -0,0 +1,4 @@
+int list_tasks(FILE *fp);
+int create_tasks(char task, FILE *fp);
+int delete_tasks(char task, FILE *fp);
+int todo_help();
