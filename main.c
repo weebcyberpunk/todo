4420cec7c82482cf0fe7f5f7055342b779ec3a95
GG <weebcyberpunk@gmail.com>
2022-03-08 19:18:07 -0300

    Corrected string comparison on main()

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 #include"todo.h"
+#include<string.h>
 
 int main(int argv, char *argc[]) {
 	// eval args
-	if ((argc[1] == "--list") || (argc[1] == "-l")) list_tasks();
-	else if ((argc[1] == "--create") || (argc[1] == "-c")) create_task(argc[2]);
-	else if ((argc[1] == "--delete") || (argc[1] == "-d")) delete_task(argc[2]);
-	else if (argc[1] == "--help") todo_help();
-	else list();
+	if ((strcmp(argc[1], "--list") == 0) || (strmp(argc[1], "-l") == 0)) list_tasks();
+	else if ((strcmp(argc[1], "--computer") == 0) || (strcmp(argc[1], "-c"))) create_task(argc[2]);
+	else if ((strcmp(argc[1], "--delete") == 0) || (strcmp(argc[1], "-d") == 0)) delete_task(argc[2]);
+	else if (strcmp(argc[1], "--help") == 0) todo_help();
+	else list_tasks();
 	
 	return(0);
 }
