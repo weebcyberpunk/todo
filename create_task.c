6819fa81a35b84e56fb4fc95ed0db9f4ff6c3048
GG <weebcyberpunk@gmail.com>
2022-03-08 19:20:00 -0300

    Added create_task()

---
+++ create_task.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+FILE *open_todo_file(char mode[]);
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 8, 2022
+ */
+int create_task(char task[]) {
+	FILE *fp;
+	fp = open_todo_file("a");
+
+	for (int count = 0;; count++) {
+		if task[count] == '\n' {
+			fprintf(stdout, "Error: Task cannot contain line breaks.\n");
+			exit(1);
+		}
+	}
+
+	fprintf(fp, "%s\n", task);
+	
+	return(0);
+}
