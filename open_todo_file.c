752aafcdf50b3e7dd7821e76c3e724bf35333229
GG <weebcyberpunk@gmail.com>
2022-03-08 19:19:03 -0300

    Added error handle in open_todo_file()

--- open_todo_file.c
+++ open_todo_file.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-FILE *open_todo_file(char mode) {
+FILE *open_todo_file(char mode[]) {
 	// getting todo.txt location
 	char *home[] = { getenv("HOME"), NULL };
 	char todo_file_name[25] = "/.local/share/todo.txt";
@@ -9,6 +10,13 @@ FILE *open_todo_file(char mode) {
 	strcat(*home, todo_file_name);
 
 	FILE *fp = fopen(*home, mode);
+
+	// error
+	if (fp == NULL) {
+		fprintf(stdout, "cannot open ~/.local/share/todo.txt");
+		exit(1);
+
+	}
 
 	return(fp);
 }
