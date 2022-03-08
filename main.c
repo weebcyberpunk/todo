0d6d63cbbf9a589e22b51f3e858db4b9739d9b13
GG <weebcyberpunk@gmail.com>
2022-03-08 19:23:30 -0300

    Corrected create arg on main()

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(int argv, char *argc[]) {
 	// eval args
 	if ((strcmp(argc[1], "--list") == 0) || (strmp(argc[1], "-l") == 0)) list_tasks();
-	else if ((strcmp(argc[1], "--computer") == 0) || (strcmp(argc[1], "-c"))) create_task(argc[2]);
+	else if ((strcmp(argc[1], "--create") == 0) || (strcmp(argc[1], "-c"))) create_task(argc[2]);
 	else if ((strcmp(argc[1], "--delete") == 0) || (strcmp(argc[1], "-d") == 0)) delete_task(argc[2]);
 	else if (strcmp(argc[1], "--help") == 0) todo_help();
 	else list_tasks();
