d2c457a9812cb7be59badb82086338a2605a89cc
GG <weebcyberpunk@gmail.com>
2022-03-08 22:08:20 -0300

    Removed comments incorrectly commited

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ int main(int argv, char *argc[]) {
 		// eval args
 		if ((strcmp(argc[1], "--list") == 0) || (strcmp(argc[1], "-l") == 0)) list_tasks();
 		else if ((strcmp(argc[1], "--create") == 0) || (strcmp(argc[1], "-c") == 0)) create_task(argc[2]);
-		/* else if ((strcmp(argc[1], "--delete") == 0) || (strcmp(argc[1], "-d") == 0)) delete_task(argc[2]); */
-		/* else if (strcmp(argc[1], "--help") == 0) todo_help(); */
-		/* else todo_help(); */
+		else if ((strcmp(argc[1], "--delete") == 0) || (strcmp(argc[1], "-d") == 0)) delete_task(argc[2]);
+		else if (strcmp(argc[1], "--help") == 0) todo_help();
+		else todo_help();
 
 	}
 	
