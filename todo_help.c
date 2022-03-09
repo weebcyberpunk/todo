d77e55da49fa2195641763b0b19d3c37e340be2f
GG <weebcyberpunk@gmail.com>
2022-03-09 01:16:13 -0300

    Added todo_help()

---
+++ todo_help.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 09, 2022
+ */
+int todo_help() {
+
+	// simply concatenate the help file to stdout
+	
+	FILE *fp;
+	fp = fopen("/usr/share/todo/help.txt", "r");
+
+	if (fp == NULL) {
+		fprintf(stdout, "cannot open help file in /usr/share/todo/help.txt\n");
+		exit(1);
+
+	}
+
+	while ((c = fgetc(fp)) != EOF) printf("%c", c);
+	
+	return(0);
+}
