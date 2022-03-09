48d5e14b3b75c1ae0776be6744a946d506713874
GG <weebcyberpunk@gmail.com>
2022-03-08 21:45:31 -0300

    Added list_tasks()

---
+++ list_tasks.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+FILE *open_todo_file(char mode[]);
+
+/*
+ * author GG weebcyberpunk@gmail.com
+ * version 0.0.0
+ * since Mar 08, 2022
+ */
+int list_tasks() {
+
+	FILE *fp;
+	fp = open_todo_file("r");
+	int count = 1;
+	int print = 1;
+	int have_tasks = 0;
+	char c;
+
+	for (;;) {
+		c = fgetc(fp);
+
+		if (c == EOF) break;
+		if (have_tasks == 0) have_tasks = 1;
+
+		if (print == 1) {
+			printf("%i - ", count);
+			print = 0;
+			count++;
+
+		}
+
+		if (c == '\n') print = 1;
+		printf("%c", c);
+
+	}
+
+	if (have_tasks == 0) printf("you completed all of your tasks! <3\n");
+	
+	return(0);
+}
