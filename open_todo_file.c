d50732c4ec69c3720363560247a22adf83873a41
GG <weebcyberpunk@gmail.com>
2022-03-19 23:23:38 -0300

    Corrected error causing err in open_todo_file() printing to stdout

--- open_todo_file.c
+++ open_todo_file.c
@@ -13,8 +13,8 @@ FILE *open_todo_file(char mode[]) {
 
 	// error
 	if (fp == NULL) {
-		fprintf(stdout, "cannot open ~/.local/share/todo.txt\n");
-		fprintf(stdout, "maybe file is not created? try creating a task with --create, it should creates the file.\n");
+		fprintf(stderr, "cannot open ~/.local/share/todo.txt\n");
+		fprintf(stderr, "maybe file is not created? try creating a task with --create, it should creates the file.\n");
 		exit(1);
 
 	}
