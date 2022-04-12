47f0da36d35be2801cffe26c63a8b3e9f1f10ee5
GG <weebcyberpunk@gmail.com>
2022-04-12 06:34:22 -0300

    Updated license in main.c

--- main.c
+++ main.c
@@ -2,12 +2,25 @@
 #include<string.h>
 
 /*
- * Todo is licensed under the GNU GPLv3 and the source code is 
- * avaiable at https://www.github.com/weebcyberpunk/todo 
- *
- * This program comes with absolutely no warranty. See the GNU GPLv3 at
- * https://www.gnu.org/licenses/gpl-3.0.html or the copy at the online
- * repository of Todo for details.
+ * Copyright 2022 © GG
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the “Software”), to deal 
+ * in the Software without restriction, including without limitation the rights to 
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ * of the Software, and to permit persons to whom the Software is furnished to do 
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
  */
 
 /*
